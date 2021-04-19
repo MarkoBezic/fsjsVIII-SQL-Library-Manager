239fea9cff6a600896a8e4774b00daf807d8a9e8
markobezic <marko.bezic@gmail.com>
2021-04-19 15:27:35 -0400

    serve static files, handle errors, pull data from books table

--- app.js
+++ app.js
@@ -5,24 +5,25 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-const sequelize = require("sequelize");
+var booksRouter = require("./routes/books");
 const db = require("./models");
 
 var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.set("view engine", "pug");
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+
+// middleware to serve up static files
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 //async IIFE
 (async () => {
@@ -38,18 +39,32 @@ app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  const err = new Error("404 Not Found");
+  err.status = 404;
+  err.message = "Sorry! The page you are looking for cannot be found.";
+  next(err);
 });
 
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+// 404 error handler
+app.use((req, res, next) => {
+  res.render("page-not-found", { err });
+});
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+//global error handler
+app.use(function (err, req, res, next) {
+  if (err.status === 404) {
+    res.status(404).render("page-not-found", { err });
+  } else {
+    // set locals, only providing error in development
+    res.locals.message =
+      err.message || "Sorry! There was an unexpected error on the server.";
+    res.locals.error = req.app.get("env") === "development" ? err : {};
+    // set status
+    res.status(err.status || 500);
+    console.log(err.status, err.message);
+    // render the error page
+    res.render("error", { err });
+  }
 });
 
 module.exports = app;
