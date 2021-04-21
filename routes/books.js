7ddfe2c090e808ab754242f07bcb6b3e01c329b9
markobezic <marko.bezic@gmail.com>
2021-04-21 10:58:53 -0400

    add search functionality and POST route for /books

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 const Book = require("../models").Book;
+const { Op } = require("sequelize");
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb) {
@@ -20,6 +21,40 @@ router.get(
   asyncHandler(async (req, res) => {
     const books = await Book.findAll();
     console.log(books);
+    res.render("books", { books });
+  })
+);
+
+/* Post searched books listing */
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const books = await Book.findAll({
+      where: {
+        [Op.or]: [
+          {
+            title: {
+              [Op.substring]: `${req.body.search}`,
+            },
+          },
+          {
+            author: {
+              [Op.substring]: `${req.body.search}`,
+            },
+          },
+          {
+            genre: {
+              [Op.substring]: `${req.body.search}`,
+            },
+          },
+          {
+            year: {
+              [Op.substring]: `${req.body.search}`,
+            },
+          },
+        ],
+      },
+    });
     res.render("books", { books });
   })
 );
